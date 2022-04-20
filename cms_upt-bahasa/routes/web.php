fa36ee08ef5eafe4642995d0db85b5ecf9dc2dbc
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2022-04-20 21:18:31 +0700

    Update web.php

--- cms_upt-bahasa/routes/web.php
+++ cms_upt-bahasa/routes/web.php
@@ -25,12 +25,7 @@ Route::prefix('admin')
         Route::get('/teacher/add', 'TeacherController@add')
             ->name('teacher.add');
 <<<<<<< HEAD
-Route::get('/news/news', 'NewsController@index')
-->name('news');
-Route::get('/news/add', 'NewsController@add')
-->name('news.add');
-Route::get('/news/edit', 'NewsController@edit')
-->name('news.edit');
+
 =======
         Route::get('/teacher/edit', 'TeacherController@edit')
             ->name('teacher.edit');
@@ -38,5 +33,11 @@ Route::get('/news/edit', 'NewsController@edit')
             ->name('internal');
         Route::get('member/internal/add', 'InternalController@add')
             ->name('internal.add');
+        Route::get('/news/news', 'NewsController@index')
+            ->name('news');
+        Route::get('/news/add', 'NewsController@add')
+            ->name('news.add');
+        Route::get('/news/edit', 'NewsController@edit')
+            ->name('news.edit');
 >>>>>>> 948bfe5041c8156288d5504ef4cfac49a65dc2fd
     });
