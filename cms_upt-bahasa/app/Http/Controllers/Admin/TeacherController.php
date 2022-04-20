ae64d5aee759aded386e69caa198d4da915fe0ca
Adilah Qurrotu'aini <adilah.qurrotu.aini@gmail.com>
2022-04-20 11:17:43 +0700

    add crud teaching staff

---
+++ cms_upt-bahasa/app/Http/Controllers/Admin/TeacherController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index(Request $request){
+        return view('pages.admin.teacher.index');
+    }
+    public function add(Request $request){
+        return view('pages.admin.teacher.add');
+    }
+}
