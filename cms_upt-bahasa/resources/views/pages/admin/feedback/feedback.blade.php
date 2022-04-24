29758a05e6dd0806cb6d83e9211dc404b2f437b3
Adilah Qurrotu'aini <adilah.qurrotu.aini@gmail.com>
2022-04-24 18:41:29 +0700

    adding feedback

---
+++ cms_upt-bahasa/resources/views/pages/admin/feedback/feedback.blade.php
@@ -0,0 +1,81 @@
+@@ -0,0 +1,85 @@
+@extends('layouts.admin.admin')
+
+@section('titlePage', 'UPT Bahasa | Feedback')
+@section('content')
+                    <!-- Main Content -->
+                <div class="main-content">
+                    <section class="section">
+                        <div class="section-header">
+                            <h1>Feedback</h1>
+                            <div class="section-header-breadcrumb">
+                                <div class="breadcrumb-item">
+                                    <a>Feedback</a>
+                                </div>
+                                <!-- <div class="breadcrumb-item">DataTables</div> -->
+                            </div>
+                        </div>
+
+                        <div class="section-body">
+                            <!-- <h2 class="section-title">DataTables</h2>
+                            <p class="section-lead">
+                                We use 'DataTables' made by @SpryMedia. You can
+                                check the full documentation
+                                <a href="https://datatables.net/">here</a>.
+                            </p> -->
+
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h4>Feedback</h4>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="table-responsive">
+                                                <table
+                                                    class="table table-striped"
+                                                    id="table-1">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>No</th>
+                                                            <th>User</th>
+                                                            <th>Feedback</th>
+                                                            <th>Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1.</td>
+                                                            <td>Kei Takebuchi</td>
+                                                            <td>This course very good</td>
+                                                            <td>
+                                                                <button
+                                                                    class="btn btn-danger"
+                                                                    data-confirm="Delete Data Feedback|Do you want to delete this data?"
+                                                                    data-confirm-yes="alert('Deleted');"
+                                                                >Delete</button>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2.</td>
+                                                            <td>Salsabila Purnami</td>
+                                                            <td>Thank You! I got highest score TOEFL teest.</td>
+                                                            <td>
+                                                                <button
+                                                                    class="btn btn-danger"
+                                                                    data-confirm="Delete Data Feedback|Do you want to delete this data?"
+                                                                    data-confirm-yes="alert('Deleted');"
+                                                                >Delete</button>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+@endsection
