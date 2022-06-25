6bd5c86f4e007cad41fdd1e5d0080ff2f3106f48
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2022-06-25 23:50:21 +0700

    content visimisi

--- cms_upt-bahasa/resources/views/pages/landing-page/visimisi.blade.php
+++ cms_upt-bahasa/resources/views/pages/landing-page/visimisi.blade.php
@@ -9,5 +9,56 @@
                 <h4 class="fw-bold text-primary text-uppercase">UPT Bahasa Politeknik Negeri Jember</h4>
             </div>
         </div>
+
+        <div class="container py-5">
+        <div class="p-5">
+            <div class="d-flex">
+                <div class="col-8">
+                    <h4 class="mb-3">VISI 2020-2024</h4>
+                
+                        <p>
+                            "Mendukung Visi dan Misi Kementrian Pendidikan
+                             dan Kebudayaan dengan Mewujudkan </br> Pendidikan 
+                             Tinggi Vokasi Yang Unggul dan Berdaya Saing 
+                             Dibidang Teknologi Yang </br> Menghasilkan Lulusan 
+                             Berkarakter"
+                        </p> 
+                        
+                    
+                </div>
+              
+            </div>
+            <div class="d-flex">
+            <div class="col-8">
+                <h4 class="mb-3 mt-5">MEWUJUDKAN VISI 2020-2035</h4>
+
+                    <p>
+                       "Menjadi Politeknik Unggul Di Asia Tahun 2035"
+                    </p>   
+            </div>
+            </div>      
+            <div class="d-flex">
+
+                <div class="col-8">
+                    <h4 class="mb-3 mt-5">MISI</h4>
+                
+                        <p>
+                            1. Meningkatkan Pendidikan Terapan Yang Inovatif Dan Berdaya Saing; </br>
+                            2. Meningkatkan Penelitian Terapan, Pengabdian Kepada Masyarakat Dan Kewirausahaan Untuk Menghasilkan Nilai Tambah Produk Inovasi; <br>
+                            3. Mewujudkan Tata Kelola Polije Yang Lebih Baik Dalam Rangka Reformasi Birokrasi (Good Polije Governance); <br>
+                            4. Mengembangkan Kerjasama Tingkat Nasional Maupun Internasional. <br>
+                        </p>   
+                </div>
+            </div>          
+
+            
+
+                
+                                    
+                                    
+         
+                                
+        </div>
+        </div>
     </div>
 @endsection
